b3b8c60623b2d36f0a067bea4a7d36cbb983376f
Meckiul Chisim <meckiulchisim@gmail.com>
2026-08-29 12:31:13 +0530

    First funtionality working

--- src/components/rows/FiftRow.jsx
+++ src/components/rows/FiftRow.jsx
@@ -30,7 +30,7 @@ const FiftRow = ({setDisplay}) => {
                 {row.icon}
             </button>
         ))}
-        <button className="w-15 h-15 p-4 rounded-full text-red-500 hover:bg-slate-400 shadow-md text-lg m-1.5 transition ease-out cursor-pointer">
+        <button onClick={() => setDisplay(equal.icon)} className="w-15 h-15 p-4 rounded-full text-red-500 hover:bg-slate-400 shadow-md text-lg m-1.5 transition ease-out cursor-pointer">
             {equal.icon}
         </button>
     </div>
