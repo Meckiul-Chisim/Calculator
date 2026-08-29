7aa55c5958bdbcb92e4b97efc15aa2bdab1ae617
Meckiul Chisim <meckiulchisim@gmail.com>
2026-08-29 12:46:12 +0530

    Added DEL clear all function

--- src/components/rows/FirstRows.jsx
+++ src/components/rows/FirstRows.jsx
@@ -26,7 +26,16 @@ const FirstRows = ({setDisplay}) => {
   return (
     <div className='w-full flex justify-evenly items-center'>
         {rows.map((row) => (
-            <button onClick={() => setDisplay( display => display + row.icon)} className="w-15 h-15 p-4 rounded-full text-red-500 cursor-pointer hover:bg-slate-400 shadow-md text-lg m-2 transition ease-out">
+            <button onClick={() => {
+                if(row.name === "allClear"){
+                    setDisplay("00")
+                } else if (row.name === "delete"){
+                    setDisplay(display => display.slice(0, -1))
+                } else {
+                    setDisplay(display => display + row.icon)
+                }
+            }} 
+            className="w-15 h-15 p-4 rounded-full text-red-500 cursor-pointer hover:bg-slate-400 shadow-md text-lg m-2 transition ease-out">
                 {row.icon}
             </button>
         ))}
