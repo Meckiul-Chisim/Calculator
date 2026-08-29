d1b8940392fd8378d93efd8ccb55d8489d45eff6
Meckiul Chisim <meckiulchisim@gmail.com>
2026-08-29 12:55:42 +0530

    prevent starting with 00

--- src/components/rows/FiftRow.jsx
+++ src/components/rows/FiftRow.jsx
@@ -26,7 +26,7 @@ const FiftRow = ({setDisplay}) => {
   return (
     <div className='w-full flex justify-evenly items-center'>
         {rows.map((row) => (
-            <button onClick={() => setDisplay(display => display + row.icon)} className="w-15 h-15 p-4 rounded-full text-slate-900 hover:bg-slate-400 shadow-md text-lg m-1.5 transition ease-out cursor-pointer">
+            <button onClick={() => setDisplay(display => display === "00" ? row.icon : display + row.icon)} className="w-15 h-15 p-4 rounded-full text-slate-900 hover:bg-slate-400 shadow-md text-lg m-1.5 transition ease-out cursor-pointer">
                 {row.icon}
             </button>
         ))}
--- src/components/rows/ThirdRow.jsx
+++ src/components/rows/ThirdRow.jsx
@@ -26,7 +26,7 @@ const ThirdRow = ({setDisplay}) => {
   return (
     <div className='w-full flex justify-evenly items-center'>
         {rows.map((row) => (
-            <button onClick={() => setDisplay(display => display + row.icon)} className="w-15 h-15 p-4 cursor-pointer rounded-full text-slate-900 hover:bg-slate-400 shadow-md text-lg m-1.5 transition ease-out">
+            <button onClick={() => setDisplay(display => display === "00" ? row.icon : display + row.icon)} className="w-15 h-15 p-4 cursor-pointer rounded-full text-slate-900 hover:bg-slate-400 shadow-md text-lg m-1.5 transition ease-out">
                 {row.icon}
             </button>
         ))}
