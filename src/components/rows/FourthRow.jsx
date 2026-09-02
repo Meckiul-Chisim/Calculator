ee58b619657d09d59a3544f9534b84e30b742e80
Meckiul Chisim <meckiulchisim@gmail.com>
2026-09-02 10:38:02 +0530

    Fix number input state in fourth calculator row

--- src/components/rows/FourthRow.jsx
+++ src/components/rows/FourthRow.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
 
 const rows = [
-    {
-        name: "one",
-        icon: "1"
-    },
-
-    {
-        name: "two",
-        icon: "2"
-    },
-
-    {
-        name: "three",
-        icon: "3"
-    },
+    { name: "one", icon: "1" },
+    { name: "two", icon: "2" },
+    { name: "three", icon: "3" },
 ]
 
 const plus = {
@@ -22,15 +11,15 @@ const plus = {
     icon: "+"
 }
 
-const FourthRow = ({setDisplay, handleOperator}) => {
+const FourthRow = ({handleNumber, handleOperator}) => {
   return (
     <div className='w-full flex justify-evenly items-center'>
         {rows.map((row) => (
-            <button onClick={() => setDisplay(display => display === "00" ? row.icon : display + row.icon)} className="w-15 h-15 p-4 rounded-full cursor-pointer text-slate-900 hover:bg-slate-400 shadow-md text-lg m-1.5 transition ease-out">
+            <button key={row.name} onClick={() => handleNumber(row.icon)} className="w-15 h-15 p-4 rounded-full cursor-pointer text-slate-900 hover:bg-slate-400 shadow-md text-lg m-1.5 transition ease-out">
                 {row.icon}
             </button>
         ))}
-        <button onClick={() => handleOperator(plus.icon)} className="w-15 h-15 p-4 rounded-full cursor-pointer text-red-500 hover:bg-slate-400 shadow-md text-lg m-1.5 transition ease-out">
+        <button onClick={() => handleOperator(plus.icon)} className="w-15 h-15 p-4 rounded-full text-red-500 hover:bg-slate-400 shadow-md text-lg m-1.5 transition ease-out cursor-pointer">
             {plus.icon}
         </button>
     </div>
